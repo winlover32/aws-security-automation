c7e5cdfa812195f9a0dc416bc185798140bd18d5
deenadat <deenadat@amazon.com>
2018-06-19 10:32:08 +1000

    removed comments and updated hard coded values
    S3 bucket was hard coded and now changed to custom one

--- EC2 Auto Clean Room Forensics/Lambda-Functions/runForensicAnalysis.py
+++ EC2 Auto Clean Room Forensics/Lambda-Functions/runForensicAnalysis.py
@@ -19,12 +19,12 @@ ssmclient = boto3.client('ssm')
 
 def lambda_handler(event, context):
     
-    # Sample Command - commands = ['sudo mkdir /forensics','sudo dd if=/dev/sda2 of=/forensics/i-23sdf5esdf.dd' ,'fls -r -m -i /forensics/i-23sdf5esdf.dd >/home/ubuntu/file-full-i-23sdf5esdf.txt', 'fls -rd /forensics/i-23sdf5esdf.dd']instance-forensics-syd-469306637372-dy2kenr1
+    
     instanceID = event['instanceID']
-    # Sample Command -  commands = ['#!/bin/bash','printf -v date "%(%F)T" -1', 'sudo mkdir /forensics','dd if=/dev/xvdb1 of=/forensics/i-23sdf5esdf.dd' ,'fls -r -m -i /forensics/i-23sdf5esdf.dd >/home/ubuntu/file-full-i-23sdf5esdf.txt', 'mactime -b /home/ubuntu/file-full-i-23sdf5esdf.txt $date >/home/ubuntu/file-2018-i-23sdf5esdf.txt', 'fls -rd /forensics/i-23sdf5esdf.dd', 'sudo apt-get install cloud-utils ','EC2_INSTANCE_ID=$(ec2metadata --instance-id)', 'cp /home/ubuntu/file-2018-i-23sdf5esdf.txt /home/ubuntu/$EC2_INSTANCE_ID.txt', 'cp /home/ubuntu/file-full-i-23sdf5esdf.txt file-full-$EC2_INSTANCE_ID.txt', 'aws s3 cp file-full-$EC2_INSTANCE_ID.txt s3://sydney-summit-incident-response/incident-response/file-full-$EC2_INSTANCE_ID.txt', 'aws s3 cp $EC2_INSTANCE_ID.txt s3://sydney-summit-incident-response/incident-response/$EC2_INSTANCE_ID.txt']
-    commands = ['#!/bin/bash','printf -v date "%(%F)T" -1', 'sudo mkdir /forensics','dd if=/dev/xvdb1 of=/forensics/i-23sdf5esdf.dd' ,'fls -r -m -i /forensics/i-23sdf5esdf.dd >/home/ubuntu/file-full-i-23sdf5esdf.txt', 'mactime -b /home/ubuntu/file-full-i-23sdf5esdf.txt $date >/home/ubuntu/file-2018-i-23sdf5esdf.txt', 'fls -rd /forensics/i-23sdf5esdf.dd >/home/ubuntu/file-deleted-i-23sdf5esdf.txt', 'sudo apt-get install cloud-utils ','EC2_INSTANCE_ID=$(ec2metadata --instance-id)', 'cp /home/ubuntu/file-deleted-i-23sdf5esdf.txt /home/ubuntu/file-deleted-$EC2_INSTANCE_ID-' + instanceID+ '.txt', 'cp /home/ubuntu/file-2018-i-23sdf5esdf.txt /home/ubuntu/$EC2_INSTANCE_ID.txt', 'cp /home/ubuntu/file-full-i-23sdf5esdf.txt /home/ubuntu/file-full-$EC2_INSTANCE_ID.txt', 'aws s3 cp /home/ubuntu/file-full-$EC2_INSTANCE_ID.txt s3://instance-forensics-syd-469306637372-dy2kenr1/incident-response/file-full-$EC2_INSTANCE_ID.txt','aws s3 cp /home/ubuntu/file-deleted-$EC2_INSTANCE_ID-' + instanceID+ '.txt s3://instance-forensics-syd-469306637372-dy2kenr1/incident-response/file-deleted-$EC2_INSTANCE_ID-' + instanceID+ '.txt', 'aws s3 cp /home/ubuntu/$EC2_INSTANCE_ID.txt s3://instance-forensics-syd-469306637372-dy2kenr1/incident-response/$EC2_INSTANCE_ID.txt']
-    # Sample Command -  commands = ['#!/bin/bash','printf -v date "%(%F)T" -1', 'sudo mkdir /forensics','dd if=/dev/xvdb1 of=/forensics/i-23sdf5esdf.dd' ,'fls -r -m -i /forensics/i-23sdf5esdf.dd >/home/ubuntu/file-full-i-23sdf5esdf.txt', 'mactime -b /home/ubuntu/file-full-i-23sdf5esdf.txt $date >/home/ubuntu/file-2018-i-23sdf5esdf.txt', 'fls -rd /forensics/i-23sdf5esdf.dd', 'sudo apt-get install cloud-utils ','EC2_INSTANCE_ID=$(ec2metadata --instance-id)', 'cp /home/ubuntu/file-2018-i-23sdf5esdf.txt /home/ubuntu/$EC2_INSTANCE_ID.txt', 'cp /home/ubuntu/file-full-i-23sdf5esdf.txt /home/ubuntu/file-full-$EC2_INSTANCE_ID.txt', 'aws s3 cp /home/ubuntu/file-full-$EC2_INSTANCE_ID.txt s3://instance-forensics-syd-469306637372-dy2kenr1/incident-response/file-full-$EC2_INSTANCE_ID.txt', 'aws s3 cp /home/ubuntu/$EC2_INSTANCE_ID.txt s3://instance-forensics-syd-469306637372-dy2kenr1/incident-response/$EC2_INSTANCE_ID.txt']
-    # Sample Command -  commands = ['#!/bin/bash','printf -v date "%(%F)T" -1', 'sudo mkdir /forensics','dd if=/dev/xvdb1 of=/forensics/i-23sdf5esdf.dd' ,'fls -r -m -i /forensics/i-23sdf5esdf.dd >/home/ubuntu/file-full-i-23sdf5esdf.txt', 'mactime -b /home/ubuntu/file-full-i-23sdf5esdf.txt $date >/home/ubuntu/file-2018-i-23sdf5esdf.txt', 'fls -rd /forensics/i-23sdf5esdf.dd', 'sudo apt-get install cloud-utils ','EC2_INSTANCE_ID=$(ec2metadata --instance-id)', 'cp /home/ubuntu/file-2018-i-23sdf5esdf.txt /home/ubuntu/$EC2_INSTANCE_ID.txt', 'cp /home/ubuntu/file-full-i-23sdf5esdf.txt file-full-$EC2_INSTANCE_ID.txt', 'aws s3 cp file-full-$EC2_INSTANCE_ID.txt s3://sydney-summit-incident-response/incident-response/file-full-$EC2_INSTANCE_ID.txt', 'aws s3 cp $EC2_INSTANCE_ID.txt s3://sydney-summit-incident-response/incident-response/$EC2_INSTANCE_ID.txt']
+    S3BucketName='sydney-summit-incident-response',
+    commands = ['#!/bin/bash','printf -v date "%(%F)T" -1', 'sudo mkdir /forensics','dd if=/dev/xvdb1 of=/forensics/i-23sdf5esdf.dd' ,'fls -r -m -i /forensics/i-23sdf5esdf.dd >/home/ubuntu/file-full-i-23sdf5esdf.txt', 'mactime -b /home/ubuntu/file-full-i-23sdf5esdf.txt $date >/home/ubuntu/file-2018-i-23sdf5esdf.txt', 'fls -rd /forensics/i-23sdf5esdf.dd >/home/ubuntu/file-deleted-i-23sdf5esdf.txt', 'sudo apt-get install cloud-utils ','EC2_INSTANCE_ID=$(ec2metadata --instance-id)', 'cp /home/ubuntu/file-deleted-i-23sdf5esdf.txt /home/ubuntu/file-deleted-$EC2_INSTANCE_ID-' + instanceID+ '.txt', 'cp /home/ubuntu/file-2018-i-23sdf5esdf.txt /home/ubuntu/$EC2_INSTANCE_ID.txt', 'cp /home/ubuntu/file-full-i-23sdf5esdf.txt /home/ubuntu/file-full-$EC2_INSTANCE_ID.txt', 'aws s3 cp /home/ubuntu/file-full-$EC2_INSTANCE_ID.txt s3://' + S3BucketName+ '/incident-response/file-full-$EC2_INSTANCE_ID.txt','aws s3 cp /home/ubuntu/file-deleted-$EC2_INSTANCE_ID-' + instanceID+ '.txt s3://'S3BucketName + '/incident-response/file-deleted-$EC2_INSTANCE_ID-' + instanceID+ '.txt', 'aws s3 cp /home/ubuntu/$EC2_INSTANCE_ID.txt s3://' + S3BucketName +'/incident-response/$EC2_INSTANCE_ID.txt']
+    
+    
     response = ssmclient.send_command(
             InstanceIds= [event.get('ForensicInstanceId')],
             DocumentName='AWS-RunShellScript',
@@ -37,15 +37,7 @@ def lambda_handler(event, context):
             OutputS3Region='ap-southeast-2',
             OutputS3BucketName='sydney-summit-incident-response',
             OutputS3KeyPrefix=event.get('ForensicInstanceId')
-            # ,
-            # ServiceRoleArn='arn:aws:iam::469306637372:role/SNSServiceRole',
-            # NotificationConfig={
-            #     'NotificationArn': 'arn:aws:sns:ap-southeast-2:469306637372:SecurityAlertMessages',
-            #     'NotificationEvents': [
-            #         'All',
-            #     ],
-            #     'NotificationType': 'Command'
-            # }
+
         )
     print (response)
     return event
