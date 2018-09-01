71c8b04616affdf7b9d054dc22923bec2ae11056
Syed Jaffry <syejaffr@acbc32c3f3af.ant.amazon.com>
2018-09-01 11:27:40 +1000

    ENHANCE: Added more Lambda env variables to make it more customisable

--- EC2 Auto Clean Room Forensics/Lambda-Functions/createForensicInstance.py
+++ EC2 Auto Clean Room Forensics/Lambda-Functions/createForensicInstance.py
@@ -29,7 +29,6 @@ def lambda_handler(event, context):
             Monitoring={
                 'Enabled': True
             },
-            # SubnetId=os.environ['SUBNET_ID'],
 
             IamInstanceProfile={
                 'Arn': os.environ['INSTANCE_PROFILE']
@@ -37,13 +36,17 @@ def lambda_handler(event, context):
             # UserData = '#!/bin/bash \n cd /tmp \n sudo yum install -y https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_386/amazon-ssm-agent.rpm \n sudo amazon-ssm-agent start',
 
             UserData = "#!/bin/bash \n export instancehostname=$(hostname) \n sudo sed -i -e 's/127.0.0.1 localhost/127.0.0.1 localhost '$instancehostname'/g' /etc/hosts \n mkdir /tmp/ssm \n cd /tmp/ssm \n wget https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/debian_amd64/amazon-ssm-agent.deb \n sudo dpkg -i amazon-ssm-agent.deb \n sudo systemctl enable amazon-ssm-agent \n sudo systemctl start amazon-ssm-agent \n",
-            KeyName ='foresic-key',
+            KeyName = os.environ['EC2_KEYPAIR'],
             NetworkInterfaces = [
-            {
-            'AssociatePublicIpAddress': True,
-            'DeviceIndex': 0,
-            'SubnetId': os.environ['SUBNET_ID']
-            }],
+                {
+                    'AssociatePublicIpAddress': True,
+                    'DeviceIndex': 0,
+                    'SubnetId': os.environ['SUBNET_ID'],
+                    'Groups': [
+                        os.environ['FORENSIC_SECUTRITYGROUP'],
+                    ],
+                }
+            ],
             TagSpecifications=[
                 {
                     'ResourceType': 'instance',
